572eb22fc50123f47223e2baa0084fef0c3f75b9
mzteo25 <164191624+mzteo25@users.noreply.github.com>
2024-03-20 16:46:37 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+   return "Welcome to the Flask CI/CD Demo! This is V2!"
+
+@app.route("/new-deployment")
+def new_deployment():
+   return "New deployment from CI/CD"
+
+@app.route("/info")
+def info():
+   user_agent = request.headers.get('User-Agent')
+   return f"Your user agent is: {user_agent}"
+
+if __name__ == "__main__":
+   app.run(debug=True)
