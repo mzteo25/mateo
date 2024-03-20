69c3409260b9ec599e50f27df0c1e0b6a8c030e2
mzteo25 <164191624+mzteo25@users.noreply.github.com>
2024-03-20 16:49:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Welcome to the Flask CI/CD Demo! This is V2!"
+   return "Modifier pour le TP!"
 
 @app.route("/new-deployment")
 def new_deployment():
